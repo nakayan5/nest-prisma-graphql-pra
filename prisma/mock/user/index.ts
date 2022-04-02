0e09d1ac2dd55e038d3398edfa4b6f6480c6cfe4
nakayan5 <tomoya.progress@gmail.com>
2022-04-02 22:21:25 +0900

    update: prisma schema

---
+++ prisma/mock/user/index.ts
@@ -0,0 +1,54 @@
+import { Prisma } from '@prisma/client';
+
+export const USER_MOCK_SAMPLE_01: Prisma.UserCreateInput[] = [
+  {
+    name: 'Alice',
+    email: 'alice@prisma.io',
+    posts: {
+      create: [
+        {
+          title: 'Join the Prisma Slack',
+          content: 'https://slack.prisma.io',
+          published: true,
+        },
+      ],
+    },
+  },
+  {
+    name: 'Nilu',
+    email: 'nilu@prisma.io',
+    posts: {
+      create: [
+        {
+          title: 'Follow Prisma on Twitter',
+          content: 'https://www.twitter.com/prisma',
+          published: true,
+        },
+      ],
+    },
+  },
+  {
+    name: 'Mahmoud',
+    email: 'mahmoud@prisma.io',
+    posts: {
+      create: [
+        {
+          title: 'Ask a question about Prisma on GitHub',
+          content: 'https://www.github.com/prisma/prisma/discussions',
+          published: true,
+        },
+        {
+          title: 'Prisma on YouTube',
+          content: 'https://pris.ly/youtube',
+        },
+      ],
+    },
+  },
+];
+
+export const USER_MOCK_SAMPLE_02: Prisma.UserCreateInput[] = [
+  {
+    name: 'sample02',
+    email: 'sample02@gmail.com',
+  },
+];
